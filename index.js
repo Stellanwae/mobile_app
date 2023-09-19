2ecc65759a6a3580a24c86f14c44f90dae92ed00
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-09-19 17:06:03 +0300

    update html and js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL:"https://grocery-mobile-app-1fde3-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
+const app = initializeApp(appSettings)
+let getGroceryDatabase = getDatabase(app)
+let nameDatabase = ref(getGroceryDatabase, "grocery-list")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    inputFieldEl.value = ""
+    push(nameDatabase, inputValue)
     
-    console.log(inputValue)
 })
